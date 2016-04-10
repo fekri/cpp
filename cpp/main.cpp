d801593804d69b5c8d3fce72c4aef7335ee4f839
fekri <fekri@me.com>
2016-04-09 21:07:46 -0400

    Initial Commit

---
+++ cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp
+//
+//  Created by Fekri kassem on 4/9/16.
+//  Copyright © 2016 Fekri kassem. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
